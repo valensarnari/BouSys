7c177593db60be4bfe2386ce46c3ed76eb50adb7
Matias Konig <matikonig13@hotmail.com>
2024-10-01 10:32:43 -0300

    Agregar registro clientes

--- codigo/gerente/actions/editar_cliente.php
+++ codigo/gerente/actions/editar_cliente.php
@@ -7,7 +7,7 @@ $nombre = $_POST['nombre'];
 $apellido = $_POST['apellido'];
 $email = $_POST['email'];
 $telefono = $_POST['telefono'];
-$contrasena = $_POST['contrasena'];
+$contrasena = password_hash($_POST['contrasena'], PASSWORD_DEFAULT);
 
 $update = "UPDATE `cliente` SET `Nombre` = '$nombre', `Apellido` = '$apellido', `Email` = '$email', `Telefono` = '$telefono', `Contrasena` = '$contrasena' WHERE `cliente`.`id` = $id";
 $query = mysqli_query($conexion, $update);
