059ade4be86f027c3b768cf3ecd602bf333c961b
hernandez matias <matias04hernandez5329@gmail.com>
2024-10-10 18:57:19 -0300

    favicom

--- pages/receptions.php
+++ pages/receptions.php
@@ -32,6 +32,7 @@ $preference->save();
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="../styles.css" rel="stylesheet">
     <script src="../script.js"></script>
+    <link rel="icon" type="image/svg+xml" href="icons/calendar-check.svg">
     <title>Reservas</title>
 </head>
 
