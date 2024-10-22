9edfaec8f7f8743677a81a86561bc5ff3d6e2a8e
hernandez matias <matias04hernandez5329@gmail.com>
2024-10-21 22:43:59 -0300

    Update quinta.php

--- codigo/recepcionista/reserva/quinta.php
+++ codigo/recepcionista/reserva/quinta.php
@@ -13,7 +13,7 @@ $sql_cliente = "SELECT nombre, apellido , puntos FROM cliente WHERE id = $reserv
 $resultado_cliente = mysqli_query($conexion, $sql_cliente);
 $cliente = mysqli_fetch_assoc($resultado_cliente);
 $puntos_cliente = $cliente['puntos'];
-echo $puntos;
+echo $puntos_cliente;
 
 $habitaciones_seleccionadas = isset($_POST['habitaciones']) ? $_POST['habitaciones'] : [];
 $habitaciones_adultos = isset($_POST['habitaciones_adultos']) ? $_POST['habitaciones_adultos'] : [];
