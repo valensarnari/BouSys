f52f619df81c84baa22230a7c2a703d1a66c7704
hernandez matias <matias04hernandez5329@gmail.com>
2024-10-22 18:40:13 -0300

    Update cancelar_reserva.php

--- codigo/recepcionista/actions/cancelar_reserva.php
+++ codigo/recepcionista/actions/cancelar_reserva.php
@@ -7,27 +7,47 @@ include('../../conexion.php');
 $reserva_id = 13;
 // Cambiar el estado de la reserva a 'Cancelada'
 $query = "UPDATE reserva_total SET Estado = 'Cancelada' WHERE id == $reserva_id";
-//---------------------tomo dato id cliente-------------------//
-$consulta_id_cte = "SELECT id_cliente from reserva_total where reserva_total.id == $reserva_id";
-$res_con = mysqli_query($conexion, $consulta_id_cte);
-$fila_id_cte = mysqli_fetch_assoc($res_con);
-//$fila_id_cte = intval($fila_id_cte);
-var_dump($fila_id_cte);
-//-------------------ver puntaje actual cliente------------------------------------------//
-$cliente_puntos = "SELECT puntos FROM cliente WHERE id = $fila_id_cte";
-$res_ptos_cte = mysqli_query($conexion, $cliente_puntos);
-$fila_ptos_cte = mysqli_fetch_assoc($res_ptos_cte);
-//var_dump($fila_ptos_cte);
-//------------------dato id habitación-----------------------------//
-//$consulta_rsv_habitacion = "SELECT 'id_habitacion' from 'reserva_habitacion r' WHERE $reserva_id = 'r.ID_Reserva'";
-//$res = mysqli_query($conexion, $consulta_rsv_habitacion);
-//$fila_id_habitacion = mysqli_fetch_assoc( $res);
-//----------------dato puntos habitacion-----------------//
-//$queryPtosHab = "SELECT puntos FROM habitacion WHERE id = $fila_id_habitacion";
-//$res_1=mysqli_query($conexion, $queryPtosHab);
-//$fila_puntos_hab= mysqli_fetch_assoc($res_1);
-//----------------------------------------------------
-//$cliente_resta="UPDATE 'cliente' SET 'Puntos'= 'Puntos'- $fila_id_habitacion['Puntos'] WHERE cliente.id = ";
+// Obtener los puntos de la habitación
+$sql1 = "SELECT h.Puntos
+        FROM reserva_habitacion rh
+        JOIN habitacion h ON rh.ID_Habitacion = h.id
+        WHERE rh.ID_Reserva = $id_reserva";
+$result1 = $conn->query($sql1);
+
+if ($result1->num_rows > 0) {
+    $row1 = $result1->fetch_assoc();
+    $habitacion_puntos = $row1["Puntos"];
+} else {
+    echo "No se encontraron puntos para la reserva.";
+    $habitacion_puntos = 0;
+}
+//Dato puntos del cliente-----------------
+
+$sql2 = "SELECT ID_Cliente FROM reserva_total WHERE id = $id_reserva";
+$result2 = $conn->query($sql2);
+
+if ($result2->num_rows > 0) {
+    $row2 = $result2->fetch_assoc();
+    $id_cliente = $row2["ID_Cliente"];
+} else {
+    echo "No se encontró el cliente para la reserva.";
+    $id_cliente = null;
+}
+//-------restar puntos al cliente que canceló rsv----------------------//
+
+if ($id_cliente !== null) {
+    $sql3 = "UPDATE cliente SET Puntos = Puntos - $habitacion_puntos WHERE id = $id_cliente";
+    if ($conn->query($sql3) === TRUE) {
+        echo "Puntos actualizados correctamente.";
+    } else {
+        echo "Error al actualizar los puntos: " . $conn->error;
+    }
+}
+
+$conn->close();
+
+
+
 
 /*
 if (mysqli_query($conexion, $query)) {
