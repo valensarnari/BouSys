d9e018860b9110e4c7dd84675fc3f26c1e023945
hernandez matias <matias04hernandez5329@gmail.com>
2024-10-22 17:28:45 -0300

    Update cancelar_reserva.php

--- codigo/recepcionista/actions/cancelar_reserva.php
+++ codigo/recepcionista/actions/cancelar_reserva.php
@@ -1,13 +1,30 @@
 <?php
-include ('../../conexion.php');
+include('../../conexion.php');
 include("../registro_login/validacion_sesion.php");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $reserva_id = $_POST['reserva_id'];
 
     // Cambiar el estado de la reserva a 'Cancelada'
-    $query = "UPDATE reserva_total SET Estado = 'Cancelada' WHERE id = '$reserva_id'";
-    
+    $query = "UPDATE 'reserva_total' SET 'Estado' = 'Cancelada' WHERE id = '$reserva_id'";
+    //---------------------tomo dato id cliente-------------------//
+    $consulta_id_cte = "SELECT id_cliente from reserva_total where reserva_total.id = $reserva_id";
+    $res_con = mysqli_query($conexion, $consulta_id_cte);
+    $fila_id_cte = mysqli_fetch_assoc($res_con);
+    echo 'id cte= ' . $fila_id_cte;
+    //-------------------sacarle los puntos al clientes------------------------------------------//
+    //------------------dato id habitación-----------------------------//
+    //$consulta_rsv_habitacion = "SELECT 'id_habitacion' from 'reserva_habitacion r' WHERE $reserva_id = 'r.ID_Reserva'";
+    //$res = mysqli_query($conexion, $consulta_rsv_habitacion);
+    //$fila_id_habitacion = mysqli_fetch_assoc( $res);
+    //----------------dato puntos habitacion-----------------//
+    //$queryPtosHab = "SELECT puntos FROM habitacion WHERE id = $fila_id_habitacion";
+    //$res_1=mysqli_query($conexion, $queryPtosHab);
+    //$fila_puntos_hab= mysqli_fetch_assoc($res_1);
+    //----------------------------------------------------
+    //$cliente_resta="UPDATE 'cliente' SET 'Puntos'= 'Puntos'- $fila_id_habitacion['Puntos'] WHERE cliente.id = ";
+
+
     if (mysqli_query($conexion, $query)) {
         header("Location: ../reservas.php");
     } else {
