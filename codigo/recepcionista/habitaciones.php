574a90e72236ed5ba1138039e55a4e4ee5d225f3
hernandez matias <matias04hernandez5329@gmail.com>
2024-10-28 20:12:25 -0300

    Update habitaciones.php

--- codigo/recepcionista/habitaciones.php
+++ codigo/recepcionista/habitaciones.php
@@ -178,7 +178,7 @@ include("../registro_login/validacion_sesion.php");
                                     // Si la habitación está ocupada, obtener datos del cliente
                                     $cliente_data = array('id' => '', 'Apellido' => '', 'Nombre' => '');
                                     if ($resultado['2'] == 'Ocupada') {
-                                        $habitacion = $resultado['0'];
+                                        $habitacion = intval($resultado['0']);
                                         $sql_cliente = "SELECT c.id, c.Nombre, c.Apellido 
                                                       FROM habitacion h
                                                       INNER JOIN reserva_habitacion rh ON h.id = rh.ID_Habitacion
@@ -193,9 +193,9 @@ include("../registro_login/validacion_sesion.php");
                                     } else {
                                         $cliente_data = array('id' => '', 'Apellido' => '', 'Nombre' => '');
                                     }
-                                    echo "<pre>";
-                                    var_dump($resultado['0']);
-                                    echo "</pre>";
+                                    /*echo "<pre>";
+                                    var_dump($habitacion);
+                                    echo "</pre>";*/
                                     ?>
                                 <tr>
                                     <td scope="row">
