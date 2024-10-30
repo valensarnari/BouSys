d0b939e08f591cb3a829beb33dd5d749af05c041
hernandez matias <matias04hernandez5329@gmail.com>
2024-10-30 12:15:14 -0300

    Update realizar_reserva.php
    hacer aquí la actualización de puntos

--- codigo/cliente/reserva/realizar_reserva.php
+++ codigo/cliente/reserva/realizar_reserva.php
@@ -15,19 +15,29 @@ try {
     $reserva_id = $_GET['reserva_id'];
     $reserva_fecha_inicio = $_GET['reserva_fecha_inicio'];
     $reserva_fecha_fin = $_GET['reserva_fecha_fin'];
-    
+
     // Decodificar arrays
     $habitaciones_seleccionadas = json_decode(base64_decode($_GET['habitaciones']), true);
     $habitaciones_adultos = json_decode(base64_decode($_GET['habitaciones_adultos']), true);
     $habitaciones_ninos = json_decode(base64_decode($_GET['habitaciones_ninos']), true);
     $habitaciones_cuna = json_decode(base64_decode($_GET['habitaciones_cuna']), true);
-    
+
     $reserva_cochera = isset($_GET['reserva_cochera']) ? $_GET['reserva_cochera'] : null;
     $valor_total = $_GET['valor_total'];
 
     $sql_reserva_total = "INSERT INTO reserva_total (ID_Cliente, Estado, Fecha_Inicio, Fecha_Fin, Fecha_Reserva, Valor_Total) VALUES (?, 'Confirmada', ?, ?, NOW(), ?)";
     $stmt_reserva_total = $conexion->prepare($sql_reserva_total);
-
+    // Preparar la consulta para actualizar los puntos del cliente
+    $sql_actualizar_puntos = "UPDATE Cliente c 
+                             JOIN Reserva_Total rt ON c.id = rt.ID_Cliente
+                             JOIN Reserva_Habitacion rh ON rt.id = rh.ID_Reserva 
+                             JOIN Habitacion h ON rh.ID_Habitacion = h.id
+                             SET c.Puntos = c.Puntos + h.Puntos
+                             WHERE rt.id = ?";
+    $stmt_actualizar_puntos = $conexion->prepare($sql_actualizar_puntos);
+    $stmt_actualizar_puntos->bind_param("i", $id_reserva_total);
+    $stmt_actualizar_puntos->execute();
+    //------------------------------------------------------
     $stmt_reserva_total->bind_param("issd", $reserva_id, $reserva_fecha_inicio, $reserva_fecha_fin, $valor_total);
     $stmt_reserva_total->execute();
     $id_reserva_total = $stmt_reserva_total->insert_id;
